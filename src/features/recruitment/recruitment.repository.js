ea14626a28e176c0f885f2c2afe081cc501be41a
Kaustubh-790 <kaustubhsharma434@gmail.com>
2026-07-03 14:37:20 +0530

    feat: implement recruitment repository with CRUD functions

---
+++ src/features/recruitment/recruitment.repository.js
@@ -0,0 +1,35 @@
+import Recruitment from "./recruitment.model.js";
+import { APPLICATION_STATUS } from "./recruitment.constants.js";
+
+export const createRecruitment = async (recruitment) => {
+  return Recruitment.create(recruitment);
+};
+
+export const getRecruitmentByUserIdAndYear = async (userId, year) => {
+  return Recruitment.findOne({ userId, year }).populate(
+    "preferredDepartmentId secondaryDepartmentId",
+  );
+};
+
+export const getRecruitmentById = async (recruitmentid) => {
+  return Recruitment.findById(recruitmentid).populate(
+    "preferredDepartmentId secondaryDepartmentId",
+  );
+};
+
+export const findAllRecruitment = async (filter) => {
+  return Recruitment.find(filter).populate(
+    "preferredDepartmentId secondaryDepartmentId",
+  ); // filter object
+};
+
+export const updateRecruitmentStatus = async (id, status) => {
+  return Recruitment.findByIdAndUpdate(id, { status }, { new: true });
+};
+
+export const findExpiredPendingPayments = async (cutoffDate) => {
+  return Recruitment.find({
+    status: APPLICATION_STATUS.PAYMENT_PENDING,
+    createdAt: { $lt: cutoffDate },
+  });
+};
