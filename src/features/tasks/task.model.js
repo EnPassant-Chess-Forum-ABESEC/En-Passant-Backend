9317df0b7fae23f40ac9efe43e44d8016da455dc
Kaustubh-790 <kaustubhsharma434@gmail.com>
2026-07-31 18:38:34 +0530

    updated task model to have assets field in tasks

--- src/features/tasks/task.model.js
+++ src/features/tasks/task.model.js
@@ -18,6 +18,12 @@ const taskSchema = new mongoose.Schema({
   instructions: [{ type: String, required: true }],
   order: { type: Number, required: true },
   isRequired: { type: Boolean, default: true },
+  assets: [
+    {
+      name: { type: String, required: true },
+      url: { type: String, required: true },
+    },
+  ],
   submission: {
     acceptsText: { type: Boolean, default: false },
     acceptsLinks: { type: Boolean, default: false },
